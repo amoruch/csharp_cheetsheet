d991f2e0e02d3a2e498e64285a74ee74a5a6f0b3
amoruch <roma.usha@yandex.ru>
2026-07-10 18:57:48 +0300

    lesson_13

---
+++ lesson_13/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            MyClass c = new MyClass();
+            c.Method();
+
+            ChildClass cc = new ChildClass();
+            cc.Method();
+        }
+    }
+
+    class MyClass {
+        public virtual void Method() {
+            Console.WriteLine("do stuff");
+        }
+    }
+
+    class ChildClass : MyClass {
+        public override void Method() {
+            Console.WriteLine("do other stuff");
+        }
+    }
+}
