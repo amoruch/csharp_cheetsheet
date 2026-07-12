5887ffb6afdddc80f4c768febb978c21c3581a9f
amoruch <roma.usha@yandex.ru>
2026-07-12 15:17:50 +0300

    lesson_15: interfaces

---
+++ lesson_15/ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("aboba");
+        }
+    }
+
+    interface IRun {
+        float speed { get; set; }
+
+        void Run(); // methods description: type, name and args
+    }
+
+    class Person : IRun {
+        public float speed { get; set; }
+
+        public void Run() {
+            Console.WriteLine("running");
+        }
+    }
+
+    // class can implement multiple interfaces
+
+    interface IJump {
+        float y { get; set; }
+
+        void jump();
+    }
+
+    class Athlet : IRun, IJump {
+        public float speed { get; set; }
+        public float y { get; set; }
+
+        public void jump() {
+            Console.WriteLine("jumping");
+        }
+
+        public void Run() {
+            Console.WriteLine("running");
+        }
+    }
+}
